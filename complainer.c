96c825a4088cd33e7814f8152250493dfcc3fbe7
Pinguim <pinguimchapeleiro@gmail.com>
2026-03-21 11:23:16 -0300

    'delimiter' and 'basic_filter' prototypes

--- complainer.c
+++ complainer.c
@@ -1,42 +1,239 @@
 #include <stdio.h>
 
-// definitions:
-#define YES 1
-#define NO 0
-#define INSIDE
-#define OUTSIDE
+/*
+ nothing, to prevent invalid values. it also tells
+ what is the current error_lines index, but when
+ there is no error_lines, its index stores...
+*/
+#define NOTHING -1
+
+// its_a_scape definitions:
+#define YES	1
+#define NO	0
+
+// comment_state && quote_state definitions:
+#define IN	1
+#define OUT	0
+
+// quotes_state definitions:
+#define OPEN	1
+#define CLOSED	0
+
+// max values:
+#define MAX_LINE_ERRORS	7
+
+// ctypes values:	\\symbolic numbers:
+#define NORMAL_CHAR	65
+#define DELIMITER	41
+#define QUOTES		96
+#define COMMENT		35
+
+// what_is_c values:	\\symbolic numbers:
+#define PARENTHESES	40
+#define BRACKETS	91
+#define BRACES		123
+
+#define DOUBLE_QUOTES	34
+#define SINGLE_QUOTES	39
+
+#define BAR		47
+#define ASTERISK	42
+
+#define SCAPE		92
+#define CHAR		97
+
 
 // external variables:
-int c;		// 'c' variable will store a single character from input
-int line = 1;
+int what_is_c, comment_state, its_a_scape;
+
+
+// external arrays:
+int error_lines[MAX_ERROR_LINES];
+
 
 // functions syntax:
+int basic_filter(int c);
+int delimiters(int c, int line);
+
 /*
-int basic_input_filter
-int delimiters
 int quotes // include scape sequence
 int comments
 */
 
 int main ()
 {
-	int value;
+	what_is_c = NOTHING;
+	comment_state = OUT;
+	its_a_scape = NO;
 
-	while ((value = basic_input_filter) == INSIDE) // while we we're inside a file
-	{
+	int c, ctype, oppened, line;
 
+	c = '\0';
+	openned = ctype = NOTHING;
+	line = 0;
 
-int basic_input_filter()
-{
-	c = getchar();
-
-	if (c == EOF)
-	{
-		return OUTSIDE;
-	}
-	else if (c == '\n')
+	while ((c = getchar()) != EOF)
 	{
 		++line;
-		return INSIDE;
+
+		// tells what is "c"
+		ctype = basic_filter(c);
+
+		// who will handle "¢"
+		if (ctype == NORMAL_CHAR)
+		{
+			canprint = printable;
+		}
+		else if (ctype == DELIMITER && comment_state == OUT && its_a_scape == NO)
+		{
+			delimiter(c, line);
+		}
+		else if (ctype == QUOTES)
+		{
+			quotes(c, line);
+		}
+		else if (ctype == COMMENT)
+		{
+			comment(c, line);
+		}
+
+		if (canprint == YES)
+		{
+			putchar(c);
+		}
 	}
+
+	putchar('\n')
+	return 0;
+}
+
+int basic_filter(int c)
+{
+/*
+	this function return the type of variable "c"
+	and tells the program what its value is in
+	order, making the functions easy to read
+*/
+
+	// delimiters
+	if (c == '(' || c == ')')
+	{
+		what_is_c = PARENTHESES;
+		return DELIMITER;
+	}
+	else if (c == '[' || c == ']')
+	{
+		what_is_c = BRACKETS;
+		return DELIMITER;
+	}
+	else if (c == '{' || c == '}')
+	{
+		what_is_c BRACES;
+		return DELIMITER;
+	}
+
+	// quotes
+	else if (c == '"')
+	{
+		what_is_c DOUBLE_QUOTES;
+		return QUOTES;
+	}
+	else if (c == '\'')
+	{
+		what_is_c SINGLE_QUOTES;
+		return QUOTES;
+	}
+
+	// comment
+	else if (c == '/')
+	{
+		what_is_c = BAR;
+		return COMMENT;
+	}
+	else if (c == '*')
+	{
+		what_is_c = ASTERISK;
+		return COMMENT;
+	}
+	else if (c == '\\')
+	{
+		what_is_c SCAPE;
+		return COMMENT;
+	}
+
+	// normal characters
+	else
+	{
+		what_is_c = CHAR;
+		return NORMAL_CHAR;
+	}
+}
+
+void delimiter(int c, int line, int oppened)
+{
+	if (what_is_c == PARENTHESES)
+	{
+		if (c == '(')
+		{
+			++openned;
+
+			if (openned < MAX_ERROR_LINES)
+			{
+				error_lines[openned] = line;
+			}
+		}
+
+		else if (c == ')' && openned >= 0)
+		{
+			--openned;
+		}
+	}
+
+	else if (what_is_c == BRACKETS)
+	{
+		if (c == '[')
+		{
+			++openned;
+
+			if (openned < MAX_ERROR_LINES)
+			{
+				error_lines[openned] = line;
+			}
+		}
+
+		else if (c == ']' && openned >= 0)
+		{
+			--openned;
+		}
+	}
+
+	else if (what_is_c == BRACES)
+	{
+		if (c == '{')
+		{
+			++openned;
+
+			if (openned < MAX_ERROR_LINES)
+			{
+				error_lines[openned] = line;
+			}
+		}
+
+		else if (c == '}' && openned >= 0)
+		{
+			--openned;
+		}
+	}
+
+	return openned;
+}
+
+
+
+
+
+
+
+
+
 
